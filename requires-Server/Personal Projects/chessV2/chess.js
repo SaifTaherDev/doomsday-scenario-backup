aa5cde970ef51f1056fbccc1da2f3dc562f4ea1c
Saif Taher <sayfo2004@gmail.com>
2018-10-08 16:11:33 +0200

    8th iteration

--- requires-Server/Personal Projects/chessV2/chess.js
+++ requires-Server/Personal Projects/chessV2/chess.js
@@ -1,12 +1,12 @@
 //Declaring all variables
 const canvas = document.body.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.height = 0.995 * screen.height;
+canvas.height = 0.9985 * screen.height;
 canvas.width = canvas.height;
-canvas.style.marginLeft = 0.21 * screen.width + "px";
-let canvasMargin = 0.21 * screen.width;
-let CW = canvas.width;
-let CH = canvas.height;
+canvas.style.marginLeft = 0.19 * screen.width + "px";
+const canvasMargin = 0.19 * screen.width;
+const CW = canvas.width;
+const CH = canvas.height;
 let playerCounter = 0;
 let squareArr = [];
 let pieceArr = [];
@@ -32,7 +32,7 @@ class square {
     }
 }
 class piece {
-    constructor(posX, posY, col, row, name, player, clr) {
+    constructor(posX, posY, col, row, name, player, img, widthFactor) {
         this.posX = posX
         this.posY = posY
         this.width = CW / 8
@@ -41,14 +41,41 @@ class piece {
         this.row = row
         this.name = name
         this.player = player
-        this.clr = clr
         this.originPosX = this.posX
         this.originPosY = this.posY
         this.lastPosX = this.posX
         this.lastPosY = this.posY
         this.moveCounter = 0
+        this.img = img
+        this.imgWidth = this.width * widthFactor
     }
 }
+//sounds & music
+const music = new Howl({
+    src: "Music_FX/chessBackgroundMusic.mp3",
+    loop: true,
+    volume: 0.5
+})
+const soundFX = [
+    new Howl({
+        src: "Music_FX/chessFX-1.mp3"
+    }),
+    new Howl({
+        src: "Music_FX/chessFX-2.mp3"
+    }),
+    new Howl({
+        src: "Music_FX/chessFX-3.mp3"
+    }),
+]
+const imgDivOne = document.body.querySelector("#deadPiecesOne .imgContainer")
+const imgDivTwo = document.body.querySelector("#deadPiecesTwo .imgContainer")
+let imagesOne = document.body.querySelectorAll("#deadPiecesOne .imgContainer img")
+let imagesTwo = document.body.querySelectorAll("#deadPiecesTwo .imgContainer img")
+let newImg, newAtt, newAttTwo;
+let scoreOneElem = document.body.querySelector("#scoreOne h1")
+let scoreTwoElem = document.body.querySelector("#scoreTwo h1")
+let scoreOne = 0
+let scoreTwo = 0
 //declaring all functions
 
 //create the board objects
@@ -62,7 +89,7 @@ function createBoard() {
         colPos = 0
         colSet = 0
         if (forCounter % 2 == 0) {
-            clrSet = "wheat"
+            clrSet = "peru"
         } else {
             clrSet = "black"
         }
@@ -71,17 +98,22 @@ function createBoard() {
             squareArr.push(new square(colPos, rowPos, clrSet, colSet, rowSet))
             colPos += CW / 8
             colSet++
-            if (clrSet == "wheat") {
+            if (clrSet == "peru") {
                 clrSet = "black"
             } else {
-                clrSet = "wheat"
+                clrSet = "peru"
             }
         }
         rowPos += CH / 8
         rowSet++
     }
 }
-
+//images
+const imgArrOne = document.body.querySelectorAll("#playerOneImg img")
+const imgArrTwo = document.body.querySelectorAll("#playerTwoImg img")
+const pawnImgOne = document.body.querySelector("#pawnPlayerOne")
+const pawnImgTwo = document.body.querySelector("#pawnPlayerTwo")
+const pieceWidthArr = [0.75, 0.75, 0.5, 0.6, 0.6, 0.5, 0.75, 0.75]
 //draw the board objects
 function drawBoard() {
     for (let square of squareArr) {
@@ -110,7 +142,7 @@ function createPieces() {
     colPos = 0
 
     for (forCounter = 0; forCounter < 8; forCounter++) {
-        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, nameArr[forCounter], "playerOne", "green"))
+        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, nameArr[forCounter], "playerOne", imgArrOne[forCounter], pieceWidthArr[forCounter]))
         colPos += CW / 8
         colSet++
     }
@@ -121,7 +153,7 @@ function createPieces() {
     colPos = 0
 
     for (forCounter = 0; forCounter < 8; forCounter++) {
-        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, "pawn", "playerOne", "green"))
+        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, "pawn", "playerOne", pawnImgOne, 0.75))
         colPos += CW / 8
         colSet++
     }
@@ -132,7 +164,7 @@ function createPieces() {
     colPos = 0
 
     for (forCounter = 0; forCounter < 8; forCounter++) {
-        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, "pawn", "playerTwo", "orange"))
+        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, "pawn", "playerTwo", pawnImgTwo, 0.75))
         colPos += CW / 8
         colSet++
     }
@@ -143,7 +175,7 @@ function createPieces() {
     colPos = 0
 
     for (forCounter = 0; forCounter < 8; forCounter++) {
-        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, nameArr[forCounter], "playerTwo", "orange"))
+        pieceArr.push(new piece(colPos, rowPos, colSet, rowSet, nameArr[forCounter], "playerTwo", imgArrTwo[forCounter], pieceWidthArr[forCounter]))
         colPos += CW / 8
         colSet++
     }
@@ -156,12 +188,7 @@ function drawPieces() {
         ctx.translate(piece.posX, piece.posY)
         ctx.fillStyle = "transparent"
         ctx.fillRect(0, 0, piece.width, piece.height)
-        ctx.fillStyle = piece.clr
-        ctx.fillRect(20, 20, 90, 90)
-        ctx.fillStyle = "black"
-        ctx.font = "12.5px Arial";
-        ctx.textAlign = "center";
-        ctx.fillText(piece.name, piece.width / 2, piece.height / 2);
+        ctx.drawImage(piece.img, (piece.width - piece.imgWidth) / 2, 0, piece.imgWidth, piece.height)
         ctx.restore()
     }
 }
@@ -421,44 +448,50 @@ function pawnMoves(squarePar, piecePar, player, moveUpDown) {
 function transformPawn(piecePar) {
     switch (promotionPrompt.toLowerCase()) {
         case "rook":
-            pieceArr[pieceArr.indexOf(piecePar)].name = "rook"
+            pieceArr[selectedPiece].name = "rook"
             validChoice = true
             break;
         case "knight":
-            pieceArr[pieceArr.indexOf(piecePar)].name = "knight"
+            pieceArr[selectedPiece].name = "knight"
             validChoice = true
             break;
         case "bishop":
-            pieceArr[pieceArr.indexOf(piecePar)].name = "bishop"
+            pieceArr[selectedPiece].name = "bishop"
             validChoice = true
             break;
         case "queen":
-            pieceArr[pieceArr.indexOf(piecePar)].name = "queen"
+            pieceArr[selectedPiece].name = "queen"
             validChoice = true
             break;
         default:
             validChoice = false
             break;
     }
+    for (let piece of pieceArr) {
+        if (piece.name == pieceArr[selectedPiece].name && piece.player == pieceArr[selectedPiece].player) {
+            pieceArr[selectedPiece].img = piece.img
+            pieceArr[selectedPiece].imgWidth = piece.imgWidth
+        }
+    }
 }
 
-function promote(piece) {
-    if (piece.name == "pawn") {
-        if (piece.player == "playerOne") {
-            if (piece.row == 7) {
+function promote(piecePar) {
+    if (piecePar.name == "pawn") {
+        if (piecePar.player == "playerOne") {
+            if (piecePar.row == 7) {
                 promotionPrompt = prompt("You've been promoted! Choose from the following ranks: 1. Queen, 2. Bishop, 3. Rook, 4. Knight")
-                transformPawn(piece)
+                transformPawn()
             }
         } else {
-            if (piece.row == 0) {
+            if (piecePar.row == 0) {
                 promotionPrompt = prompt("You've been promoted! Choose from the following ranks: 1. Queen, 2. Bishop, 3. Rook, 4. Knight")
-                transformPawn(piece)
+                transformPawn()
             }
         }
     }
     while (validChoice == false) {
         promotionPrompt = prompt("You've been promoted! Choose from the following ranks: 1. Queen, 2. Bishop, 3. Rook, 4. Knight")
-        transformPawn(piece)
+        transformPawn()
     }
 }
 function determineMoves(squarePar, piecePar) {
@@ -514,7 +547,7 @@ function determineMoves(squarePar, piecePar) {
     }
 }
 
-function movePiece(xClick, yClick, piece) {
+function movePiece(xClick, yClick, piece, imgDiv) {
     for (let square of squareArr) {
         if (xClick > square.posX && xClick < square.posX + square.width) {
             if (yClick > square.posY && yClick < square.posY + square.height) {
@@ -583,15 +616,36 @@ function movePiece(xClick, yClick, piece) {
         uponStopInterval = setInterval(uponStopExe, 1)
         if (selectedSquare.hasPiece) {
             killedPiece = selectedSquare.presentPiece
-            console.log(selectedSquare.presentPiece)
+            newImg = document.createElement("img")
+            newAtt = document.createAttribute("src")
+            newAtt.value = selectedSquare.presentPiece.img.src
+            newAttTwo = document.createAttribute("class")
+            newAttTwo.value = selectedSquare.presentPiece.img.src
+            newImg.setAttributeNode(newAtt);
+            newImg.setAttributeNode(newAttTwo);
+            imgDiv.appendChild(newImg)
             pieceArr.splice(pieceArr.indexOf(selectedSquare.presentPiece), 1)
+            if (playerCounter % 2 == 0) {
+                scoreOne++
+                scoreOneElem.innerHTML = "Player One Score: " + JSON.stringify(scoreOne)
+            } else {
+                scoreTwo++
+                scoreTwoElem.innerHTML = "Player One Score: " + JSON.stringify(scoreTwo)
+            }
         }
         piece.row = selectedSquare.row
         piece.col = selectedSquare.col
     }
 }
 function uponStopExe() {
-    verifyNoCheckBefore(pieceArr[selectedPiece], selectedSquare)
+    imagesOne = document.body.querySelectorAll("#deadPiecesOne .imgContainer img")
+    imagesTwo = document.body.querySelectorAll("#deadPiecesTwo .imgContainer img")
+    if (pieceArr[selectedPiece].player == "playerOne") {
+        verifyNoCheckBefore(pieceArr[selectedPiece], selectedSquare, imagesOne, imgDivOne)
+    } else {
+        verifyNoCheckBefore(pieceArr[selectedPiece], selectedSquare, imagesTwo, imgDivTwo)
+    }
+    
 }
 function determine(e) {
     determineSelected(e.clientX - canvasMargin, e.clientY)
@@ -600,13 +654,17 @@ function determine(e) {
 }
 
 function move(e) {
-    movePiece(e.clientX - canvasMargin, e.clientY, pieceArr[selectedPiece])
+    if (pieceArr[selectedPiece].player == "playerOne") {
+        movePiece(e.clientX - canvasMargin, e.clientY, pieceArr[selectedPiece], imgDivOne)
+    } else {
+        movePiece(e.clientX - canvasMargin, e.clientY, pieceArr[selectedPiece], imgDivTwo)
+    }
     canvas.addEventListener("click", determine)
     for (let square of squareArr) {
         square.clr = square.originClr
     }
 }
-function verifyNoCheckBefore(piecePar, squarePar) {
+function verifyNoCheckBefore(piecePar, squarePar, imgArr, element) {
     if (piecePar.posX == selectedSquare.posX && piecePar.posY == selectedSquare.posY) {
         interval = setInterval(function () {
             updateStates();
@@ -621,11 +679,20 @@ function verifyNoCheckBefore(piecePar, squarePar) {
                     if (squareHasPieceBool) {
                         pieceArr.push(killedPiece)
                     }
+                    for (let img of imgArr) {
+                        if (img.src == killedPiece.img.src && img.style.display != "none") {
+                            img.style.display = "none"
+                            break
+                        }
+                    }
                     alert("You can't check yourself!")
+                    scoreOne--
+                    scoreOneElem.innerHTML = "Player One Score: " + scoreOne
                 } else {
+                    promote(piecePar)
+                    soundFX[Math.floor(Math.random() * 2.99)].play()
                     setTimeout(function () {
                         playerCounter++
-                        promote(piece)
                         movingBoolean = false
                         for (let square of squareArr) {
                             square.clr = square.originClr
@@ -640,10 +707,13 @@ function verifyNoCheckBefore(piecePar, squarePar) {
                         pieceArr.push(killedPiece)
                     }
                     alert("You can't check yourself!")
+                    scoreTwo--
+                    scoreTwoElem.innerHTML = "Player Two Score: " + scoreTwo
                 } else {
+                    soundFX[Math.floor(Math.random() * 2.99)].play()
+                    promote(piecePar)
                     setTimeout(function () {
                         playerCounter++
-                        promote(piece)
                         movingBoolean = false
                         for (let square of squareArr) {
                             square.clr = square.originClr
@@ -918,6 +988,7 @@ function checkMate() {
     }
 }
 //the mainLoop
+music.play()
 function mainLoop() {
     drawBoard()
     drawPieces()
